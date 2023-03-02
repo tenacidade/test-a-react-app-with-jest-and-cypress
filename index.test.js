51f668d1f6e52682567d80df4f75d241ec212b48
Gabriel Duarte <grd1996@gmail.com>
2023-03-01 22:09:08 -0300

    added comments to test

--- index.test.js
+++ index.test.js
@@ -1,9 +1,32 @@
 isPalindrome = require('./index.js')
+//Require the function we want to test: isPalindrome = require('./index.js')
 
 test('neuquen is palindrom', () => {
     expect(isPalindrome("neuquen")).toBe(true)
 })
 
+
+
 test('bariloche is not palindrom', () => {
     expect(isPalindrome("bariloche")).toBe(false)
 })
+
+
+/*
+ * The test() function is provided by Jest and within it 
+ * we will put the code we want Jest to execute.
+ * 
+ * test() takes two parameters. The first is a test description, 
+ * which is a distinctive name that will show on our console when the test is run.
+ * 
+ * The second parameter is a callback, which contains the actual testing code.
+ * 
+ * Within this callback we're calling the expect() function (also provided by Jest)
+ * expect() takes our function as parameter, which itself is receiving a parameter we made up.
+ * 
+ * Last, we chain the .toBe() function (provided by Jest too) and as parameter
+ * we pass it the value we expect isPalindrome() to return for each case.
+ * 
+ * ("neuquen" is a palindrome so our function should return true, and "bariloche" is not, so it should return false.)
+ * 
+*/
