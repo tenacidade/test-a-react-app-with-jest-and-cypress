f1e7ab3d17b398ab987f6f533f356bb485f02ab0
Gabriel Duarte <grd1996@gmail.com>
2023-03-01 21:58:52 -0300

    added palindrome checker

---
+++ index.js
@@ -0,0 +1,18 @@
+function isPalindrome(string) {
+    // O(n)
+    // Put a pointer at each extreme of the word and iterate "inwards"
+    // At each iteration, check if the pointers represent equal values
+    // If this condition isn't accomplished, the word isn't a palindrome
+    let left = 0
+    let right = string.length - 1
+
+    while (left < right) {
+        if (string[left] === string[right]) {
+            left += 1
+            right -= 1
+        }
+        else return false
+    }
+
+    return true
+}
